0bd38e39a95b369d68158831fcc210af24102e46
xflynx25 <59663716+xflynx25@users.noreply.github.com>
2023-07-01 17:54:17 -0400

    fixed init directory issue

--- pyfit/datasets/gen.py
+++ pyfit/datasets/gen.py
@@ -3,9 +3,14 @@ import numpy as np
 # generate dataset for practicing machine learning tasks for different types of hoped for data
 class Gen():
     def __init__(self) -> None:
-        pass
+        self.data = 1
 
     # first dim of dimensions is number of samples. Rest is size of the data
-    def make_data(style: str, dimensions: np.array):
+    def make_data(self, style: str, dimensions: np.array):
         if style == 'random': 
-            return [[1,2],[2,3]]
+            self.data = [[1,2],[2,3]]
+        else:
+            self.data = "your mom"
+
+    def __str__(self) -> str:
+        return str(self.data)
--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name='pyfit',
     version='0.0.1',
+    packages=find_packages(),
+    install_requires=required,
     author='xflynx25',
     author_email='JFlynn250@outlook.com',
-    description='Testing installation of Package',
+    description='Pyfit, your ml toolkit',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url= 'https://github.com/xflynx25/pyfit',
@@ -19,3 +24,5 @@ setuptools.setup(
     packages=['pyfit'],
     install_requires=[],
 )
+
+from setuptools import setup, find_packages
